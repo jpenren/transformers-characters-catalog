c25f89f0029ca418fc1215c6bf8b49a6325806ab
Javier Pena <javiprendo@gmail.com>
2016-08-06 20:14:41 +0200

    Configurationo update

--- app/app.config.js
+++ app/app.config.js
@@ -1,6 +1,4 @@
 
 angular
     .module('app')
-    .constant('IMAGE_API_KEY', 'AIzaSyBz-VFSOP4nTT1_CvkcsRVcQAAW4GqAVic')
-    .constant('IMAGE_API_URL', 'https://www.googleapis.com/customsearch/v1?cx=006526179263721937451%3Apjm-ehlimhs&dateRestrict=2000&fileType=jpg&imgColorType=color&imgSize=large&searchType=image&fields=items(image%2FthumbnailLink%2Clink%2Ctitle)&key=')
-    .constant('DATA_API_URL', 'https://raw.githubusercontent.com/jpenren/transformers-characters-catalog/master/transformers.json');
+    .constant('DATA_API_URL', 'https://raw.githubusercontent.com/jpenren/transformers-characters-catalog/master/data/transformers.json');
